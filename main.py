8c2d8a3bd54de4616c8bb2d3aa71caba063048f7
magnesyljuasen <msylju@gmail.com>
2025-05-11 21:24:10 +0200

    ferdig

--- main.py
+++ main.py
@@ -17,13 +17,21 @@ from matplotlib.colors import to_hex
 import streamlit.components.v1 as components
 from folium.plugins import Fullscreen
 from folium.raster_layers import WmsTileLayer
+import geopandas as gpd
 
 
 # Step 0: Settings
 st.set_page_config(
     layout='wide',
-    initial_sidebar_state='auto'
+    initial_sidebar_state='auto',
+    page_title='Leilighetsvelger',
 )
+
+with open("main.css") as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+st.write('')
+st.write('')
 
 # Step 1: Get relevant IDs
 @st.cache_resource(show_spinner='Laster inn annonser...')
@@ -184,11 +192,10 @@ with st.sidebar:
         oppvaringskarakter = st.multiselect(label='Oppvarmingskarakter', options=df['Oppvarmingskarakter'].unique(), default=df['Oppvarmingskarakter'].unique())
         df = df[df['Oppvarmingskarakter'].isin(oppvaringskarakter)]
     #
-    st.info('Blåere farge betyr at leiligheten er dyrere. Større sirkler betyr større areal.', icon=':material/info:')
 
 
 # Step 6: Vise på kart
-cmap = plt.cm.get_cmap('Blues')
+cmap = plt.cm.get_cmap('copper_r')
 
 def value_to_color(value, min_value, max_value):
     norm = (value - min_value) / (max_value - min_value)
@@ -205,108 +212,283 @@ def format_value(value, suffix='', is_int=True):
         return f"{int(value):,}{suffix}".replace(',', ' ')
     return f"{value}{suffix}"
 
-m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
-for _, row in df.iterrows():
-    tooltip_parts = []
-    address = row['object-address'].split(',')[0] if pd.notna(row['object-address']) else ''
-    tooltip_parts.append(f"<i>{address}</i><br>")
-    fields = [
-        ("Totalpris", row['pricing-total-price'], " kr"),
-        ("Prisantydning", row['pricing-incicative-price'], " kr"),
-        ("Felleskost/mnd", row['pricing-common-monthly-cost'], " kr"),
-        ("Fellesgjeld", row['pricing-joint-debt'], " kr"),
-        ("Internt bruksareal", row['info-usable-i-area'], " m²"),
-        ("Eksternt bruksareal", row['info-usable-e-area'], " m²"),
-        ("Innglasset balkong", row['info-usable-b-area'], " m²"),
-        ("Balkong/terrasse", row['info-open-area'], " m²"),
-        ("Bruksareal", row['info-usable-area'], " m²"),
-        ("Etasje", row['info-floor'], ""),
-        ("Antall soverom", row['info-bedrooms'], ""),
-        ("Antall rom", row['info-rooms'], ""),
-        ("Byggeår", row['info-construction-year'], ""),
-        ("Eierform", row['info-ownership-type'], ""),
-        ("Kvadratmeterpris", row['square-meter-price'], " kr/m²"),
-        ("Energikarakter", row['Energikarakter'], ""),
-        ("Oppvarmingskarakter", row['Oppvarmingskarakter'], "")
-    ]
-    tooltip_parts.append("--- <br>")
-    for label, value, suffix in fields:
-        formatted = format_value(value, suffix, is_int=isinstance(value, (int, float)))
-        if formatted:
-            tooltip_parts.append(f"{label}: <strong>{formatted}</strong><br>")
-    tooltip_content = "\n".join(tooltip_parts)
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=scale_value(value=row['usable-area'], min_value=60, max_value=90),
-        color=value_to_color(value=row['pricing-total-price'], min_value=5000000, max_value=7500000),
-        fill=True,
-        fill_color=value_to_color(value=row['pricing-total-price'], min_value=5000000, max_value=7500000),
-        fill_opacity=0.5,
-        tooltip=folium.Tooltip(tooltip_content)
+@st.cache_resource(show_spinner='Viser kart...')
+def show_map(df):
+    m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12, tiles='CartoDB Positron', attr='Carto', )
+    for _, row in df.iterrows():
+        tooltip_parts = []
+        address = row['object-address'].split(',')[0] if pd.notna(row['object-address']) else ''
+        tooltip_parts.append(f"<i>{address}</i><br>")
+        fields = [
+            ("Totalpris", row['pricing-total-price'], " kr"),
+            ("Prisantydning", row['pricing-incicative-price'], " kr"),
+            ("Felleskost/mnd", row['pricing-common-monthly-cost'], " kr"),
+            ("Fellesgjeld", row['pricing-joint-debt'], " kr"),
+            ("Internt bruksareal", row['info-usable-i-area'], " m²"),
+            ("Eksternt bruksareal", row['info-usable-e-area'], " m²"),
+            ("Innglasset balkong", row['info-usable-b-area'], " m²"),
+            ("Balkong/terrasse", row['info-open-area'], " m²"),
+            ("Bruksareal", row['info-usable-area'], " m²"),
+            ("Etasje", row['info-floor'], ""),
+            ("Antall soverom", row['info-bedrooms'], ""),
+            ("Antall rom", row['info-rooms'], ""),
+            ("Byggeår", row['info-construction-year'], ""),
+            ("Eierform", row['info-ownership-type'], ""),
+            ("Kvadratmeterpris", row['square-meter-price'], " kr/m²"),
+            ("Energikarakter", row['Energikarakter'], ""),
+            ("Oppvarmingskarakter", row['Oppvarmingskarakter'], "")
+        ]
+        tooltip_parts.append("--- <br>")
+        for label, value, suffix in fields:
+            formatted = format_value(value, suffix, is_int=isinstance(value, (int, float)))
+            if formatted:
+                tooltip_parts.append(f"{label}: <strong>{formatted}</strong><br>")
+        tooltip_content = "\n".join(tooltip_parts)
+        folium.CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=scale_value(value=row['usable-area'], min_value=60, max_value=90),
+            color=value_to_color(value=row['pricing-total-price'], min_value=5000000, max_value=7500000),
+            fill=True,
+            fill_color=value_to_color(value=row['pricing-total-price'], min_value=5000000, max_value=7500000),
+            fill_opacity=0.5,
+            tooltip=folium.Tooltip(tooltip_content),
+            popup=folium.Popup(f'<a href="{row["URL"]}" target="_blank">Til annonsen</a>', max_width=300)
+        ).add_to(m)
+
+    satellite_url = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+    satellite_attribution = 'Esri, Maxar, Earthstar Geographics, and the GIS User Community'
+    folium.TileLayer(
+        tiles=satellite_url,
+        attr=satellite_attribution,
+        name='Flyfoto',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
     ).add_to(m)
 
-satellite_url = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-satellite_attribution = 'Esri, Maxar, Earthstar Geographics, and the GIS User Community'
-folium.TileLayer(
-    tiles=satellite_url,
-    attr=satellite_attribution,
-    name='Satellite',
-    overlay=True,
-    control=True
-).add_to(m)
 
-cartodb_url = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
-cartodb_attribution = 'CartoDB, OpenStreetMap contributors'
-folium.TileLayer(
-    tiles=cartodb_url,
-    attr=cartodb_attribution,
-    name='CartoDB Positron',
-    overlay=True,
-    control=True
-).add_to(m)
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/Samlet_alle_malpunkt_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Alle målepunkt',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
 
-# https://asplanviak.maps.arcgis.com/home/item.html?id=8aa6b06460394ad797a59de82dead917
-folium.LayerControl().add_to(m)
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/Skoler_barnehager_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Skoler, barnehager og lignende',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
 
-Fullscreen().add_to(m)
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/park_gront_marka_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Park, grøntområder og marka',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
+
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/lek_fritid_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Lek, fritid og lignende',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
+
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/kultur_servering_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Kultur, servering og lignende',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
+
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/handel_tjenester_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Handel, tjenester og lignende',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False
+    ).add_to(m)
+
+    folium.TileLayer(
+        tiles='https://tiles.arcgis.com/tiles/whQdER0woF1J7Iqk/arcgis/rest/services/kollektivtilbud_ol/MapServer/tile/{z}/{y}/{x}',
+        attr='AV',
+        name='Gangtilgjenglighet - Kollektivtilbud og lignende',
+        overlay=True,
+        control=True,
+        opacity=0.5,
+        show=False    
+    ).add_to(m)
+
+    url = "https://services.arcgis.com/whQdER0woF1J7Iqk/arcgis/rest/services/Jernbane_og_T_bane/FeatureServer/3/query"
+    params = {
+        "where": "1=1",
+        "outFields": "*",
+        "f": "geojson"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        geojson_data = response.json()
+        bane_group = folium.FeatureGroup(name="Bane", show=False)
+        for feature in geojson_data['features']:
+            coords = feature['geometry']['coordinates']
+            props = feature['properties']
+            navn = props.get('NAVN', 'Ukjent')
+
+            # Create a custom icon (can change icon, color, etc.)
+            icon = folium.Icon(icon='train', prefix='fa', color='red')
+
+            # Add the marker to the map
+            folium.Marker(
+                location=[coords[1], coords[0]],  # GeoJSON is (lon, lat)
+                tooltip=f'{navn} (tog)',
+                icon=icon
+            ).add_to(bane_group)
+
+    url = "https://services.arcgis.com/whQdER0woF1J7Iqk/arcgis/rest/services/Jernbane_og_T_bane/FeatureServer/1/query"
+    params = {
+        "where": "1=1",
+        "outFields": "*",
+        "f": "geojson"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        geojson_data = response.json()
+        for feature in geojson_data['features']:
+            coords = feature['geometry']['coordinates']
+            props = feature['properties']
+            navn = props.get('STRENG', 'Ukjent')
+
+            # Create a custom icon (can change icon, color, etc.)
+            icon = folium.Icon(icon='train', prefix='fa', color='blue')
+
+            # Add the marker to the map
+            folium.Marker(
+                location=[coords[1], coords[0]],  # GeoJSON is (lon, lat)
+                tooltip=f'{navn} (t-bane)',
+                icon=icon
+            ).add_to(bane_group)
+
+    url = "https://services.arcgis.com/whQdER0woF1J7Iqk/arcgis/rest/services/Jernbane_og_T_bane/FeatureServer/2/query"
+    params = {
+        "where": "1=1",
+        "outFields": "*",
+        "f": "geojson"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        geojson_data = response.json()
+        for feature in geojson_data['features']:
+            coords = feature['geometry']['coordinates']
+            props = feature['properties']
+            navn = props.get('Temakode', 'Ukjent')
+
+            if feature['geometry']['type'] == 'MultiLineString':
+                for line in coords:
+                    folium.PolyLine(
+                        locations=[(lat, lon) for lon, lat in line],
+                        color="blue",
+                        weight=2,
+                        #tooltip=navn
+                    ).add_to(bane_group)
+            elif feature['geometry']['type'] == 'LineString':
+                folium.PolyLine(
+                    locations=[(lat, lon) for lon, lat in coords],
+                    color="blue",
+                    weight=2,
+                    #tooltip=navn
+                ).add_to(bane_group)
+
+    url = "https://services.arcgis.com/whQdER0woF1J7Iqk/arcgis/rest/services/Jernbane_og_T_bane/FeatureServer/4/query"
+    params = {
+        "where": "1=1",
+        "outFields": "*",
+        "f": "geojson"
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        geojson_data = response.json()
+        lines_group = folium.FeatureGroup(name="Jernbanelinje", show=False)
+        for feature in geojson_data['features']:
+            coords = feature['geometry']['coordinates']
+            props = feature['properties']
+            navn = props.get('Temakode', 'Ukjent')
+
+            if feature['geometry']['type'] == 'MultiLineString':
+                for line in coords:
+                    folium.PolyLine(
+                        locations=[(lat, lon) for lon, lat in line],
+                        color="red",
+                        weight=2,
+                        #tooltip=navn
+                    ).add_to(bane_group)
+            elif feature['geometry']['type'] == 'LineString':
+                folium.PolyLine(
+                    locations=[(lat, lon) for lon, lat in coords],
+                    color="red",
+                    weight=2,
+                    #tooltip=navn
+                ).add_to(bane_group)
+
+    bane_group.add_to(m)
+
+    jobb_group = folium.FeatureGroup(name="Jobb", show=True)
+    coords1 = [59.9290456, 10.7367929] 
+    coords2 = [59.9110103,10.7356901] 
+    name1 = "Ullevålsveien Skole"
+    name2 = "Nedre Vollgate 4"
+    folium.Marker(
+        location=coords1,
+        tooltip=name1,
+        icon=folium.Icon(color='green', icon='briefcase')
+    ).add_to(jobb_group)
+    folium.Marker(
+        location=coords2,
+        tooltip=name2,
+        icon=folium.Icon(color='green', icon='briefcase')
+    ).add_to(jobb_group)
+    jobb_group.add_to(m)
+
+    url = 'https://wms.geonorge.no/skwms1/wms.grunnkretser?request=GetCapabilities&service=WMS'
+    folium.WmsTileLayer(
+        url = url,
+        layers = 'Grunnkretser',
+        transparent = True, 
+        control = True,
+        fmt="image/png",
+        name = 'Grunnkretser',
+        overlay = True,
+        show = True,
+        opacity = 0.2
+        ).add_to(m)
 
 
-c1, c2 = st.columns([2, 1])
-with c1:
-    with st.container(border=True, height=400):
-        st.write(f"**Det er {len(df)} leiligheter som vises på kartet**")
-        last_object_clicked = st_folium(m, use_container_width=True, returned_objects=['last_object_clicked'], height=300)
-        if last_object_clicked['last_object_clicked'] == None:
-            st.stop()
-        buffer_degrees = 60 / 111000  # ≈ 0.000045
 
-        lat = last_object_clicked['last_object_clicked']['lat']
-        lng = last_object_clicked['last_object_clicked']['lng']
+    # https://asplanviak.maps.arcgis.com/home/item.html?id=8aa6b06460394ad797a59de82dead917
+    folium.LayerControl(position='bottomleft').add_to(m)
 
-        lat_min = lat - buffer_degrees
-        lat_max = lat + buffer_degrees
-        lng_min = lng - buffer_degrees
-        lng_max = lng + buffer_degrees
+    Fullscreen(position='topright').add_to(m)
 
-        df = df[
-            (df['latitude'] >= lat_min) & (df['latitude'] <= lat_max) &
-            (df['longitude'] >= lng_min) & (df['longitude'] <= lng_max)
-        ].reset_index(drop=True)
-with c2:
-    with st.container(border=True, height=400):
-        st.write(f"**{df['object-address'][0].split(',')[0]}**")
-        url = df['URL'].iloc[0]
-        
-        st.write(f"*{df['object-title'][0]}*")
-        st.markdown(f"[Gå til annonsen]({url})", unsafe_allow_html=True)
-        date_0 = df['Visning 0'][0]
-        date_1 = df['Visning 1'][0]
-        if date_0 is not None:
-            st.write(f"Visningen er: {date_0.day}.{date_0.month} ({date_0})")
-        if date_1 is not None:
-            st.write(f"Visningen er: {date_1.day}.{date_1.month} ({date_1})")
-        
-        if pd.notna(df['preemption'][0]):
-            st.write(f"Om forkjøpsrett: <{df['preemption'][0]}>")
-        else:
-            st.write('Det er ikke forkjøpsrett')
+    st.write(f"**Det er {len(df)} leiligheter som vises på kartet**")
+    return m
+
+m = show_map(df)
+st_folium(m, use_container_width=True, returned_objects=[], height=400)
